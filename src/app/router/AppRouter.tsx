6828a7e08e40e5ee03d68b40a282eef13f0b6c32
TruongQuocHuy-dev <tqhuy.dev.frontend@gmail.com>
2026-05-11 20:23:34 +0700

    refactor(router): remove unused AdminUsersPage route from AppRouter

--- src/app/router/AppRouter.tsx
+++ src/app/router/AppRouter.tsx
@@ -1,7 +1,6 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { LoginPage } from '../../features/auth'
 import { AdminDashboardPage } from '../../features/dashboard'
-import { AdminUsersPage } from '../../features/admin/users/pages/AdminUsersPage'
 import { ProtectedAdminRoute } from './ProtectedAdminRoute'
 import { PublicRoute } from './PublicRoute'
 
@@ -11,14 +10,13 @@ export function AppRouter() {
       <Route element={<PublicRoute />}>
         <Route path="/login" element={<LoginPage />} />
       </Route>
-      
+
       <Route element={<ProtectedAdminRoute />}>
         <Route path="/admin" element={<AdminDashboardPage />} />
-        <Route path="/admin/users" element={<AdminUsersPage />} />
       </Route>
-      
+
       <Route path="/" element={<Navigate to="/admin" replace />} />
       <Route path="*" element={<Navigate to="/admin" replace />} />
     </Routes>
   )
-}
+}
